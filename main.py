75f6e09e157e96924f6b4cd50e585e46457a1808
ostephane <ostephane9@gmail.com>
2022-03-03 14:05:23 +0200

    ajout de commentaires pour un code plus lisible amelioration de la partie gestion d’erreur avec la possibilité de créer un dossier avec un nom aléatoire

--- main.py
+++ main.py
@@ -1,37 +1,39 @@
 import csv
 import os
-import shutil
 import requests
 
 from category import category_link, category_page
 from page import Page
 
-url = 'http://books.toscrape.com/catalogue/'
+# l'URL du site à scraper
+url = 'http://books.toscrape.com/'
 
-# La list qui contient sortie’en-tête des infos scrapes
+# La list qui contient l’en-tête des infos scrapes
 info_id = ['product_page_url', 'title', 'universal_ product_code (upc)',
            'price_including_tax', 'price_excluding_tax', 'number_available',
            'review_rating', 'product_description', 'category', 'image_url']
 
+# cette boucle visite chaque category de livre du site 'http://books.toscrape.com/'
 for cl in category_link:
     # récupérer tous les liens des livres de la category
-    # liens est une liste qui contient, par page, tous les liens de la category
-    liens = category_page(url.removesuffix('catalogue/') + cl, links=None)
+    # liens est une liste qui contient tous les liens des livres de la category,
+    # ces liens sont groupés par page web de la category
+    liens = category_page(url + cl, links=None)
 
     # nom_fichier est le nom que va prendre le fichier csv pour chaque category
     nom_fichier = str(cl[25:].removesuffix('/index.html')) + '.csv'
     category_infos = []  # va contenir les données de tous les livres d’une category
-    images = []  # va contenir les urls des images de couverture
+    images = []  # va contenir les urls et les titres des images de couverture
 
     # pour chaque page de la category
     for page in liens:
         # pour chaque livre de la page
         for lien in page:
             p = Page(lien)  # infos d’un seul livre
-            by_category_info = [p.url, p.title, p.upc, p.price_including_tax,
-                                p.price_excluding_tax, p.number_available, p.review_rating,
-                                p.product_description, p.category, p.image_url]
-            category_infos.append(by_category_info)
+            # ajouter les infos du livre à la liste des infos de la category
+            category_infos.append([p.url, p.title, p.upc, p.price_including_tax,
+                                   p.price_excluding_tax, p.number_available, p.review_rating,
+                                   p.product_description, p.category, p.image_url])
             images.append([p.title, p.image_url])
 
     # mettre les infos dans un fichier csv
@@ -47,10 +49,18 @@ for cl in category_link:
     # télécharger les images de couverture
     cwd = os.getcwd()
     try:
-        os.mkdir(os.path.join(cwd, nom_fichier + 'images'))
-        os.chdir(os.path.join(cwd, nom_fichier + 'images'))
-    except:
-        pass
+        new = nom_fichier + 'images'
+        os.mkdir(os.path.join(cwd, new))
+    except FileExistsError:
+        print('dans le repertoire courant, il y a un dossier qui a le même nom : ',
+              nom_fichier + 'images')
+        new = input('Veuillez entrer un autre nom manuellement : ')
+        os.mkdir(os.path.join(cwd, new))
+
+    # Entrer dans le dossier nouvellement créer pour y mettre les photos de couverture
+    os.chdir(os.path.join(cwd, new))
+
+    # scraper les photos de couverture
     for s in images:
         with open(str(s[0]) + '.jpg', 'wb+') as image_file:
             image_file.write(requests.get(s[1]).content)
